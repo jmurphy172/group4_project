5cfdca399631234cdd0e4721baace094ee4b7cfc
jmurphy172 <138017520+jmurphy172@users.noreply.github.com>
2025-04-03 15:20:21 +0100

    adding and deleting files

---
+++ cleaning.py
@@ -0,0 +1,9 @@
+
+
+
+class read_data:
+    def __init__(self, *args, **kwargs):
+        pass  # Your initialization code here
+        
+    def base_path(self):
+        return "C:/Users/James/OneDrive/Desktop/GitHub/group4_project"
