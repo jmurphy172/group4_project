d41ee0418891da9ca1b6cb4122f327a417a82fc2
jmurphy172 <138017520+jmurphy172@users.noreply.github.com>
2025-04-23 19:33:25 +0100

    Update load_data.py

--- main/load_data.py
+++ main/load_data.py
@@ -8,19 +8,21 @@ from pathlib import Path
 
 
 
-class Load_data:
+   
+def get_root_dir():
+    return read_data.root() 
+ 
+def load_raw_data():
     
-    
-    def get_root_dir():
-        return read_data.root() 
-    
-    def load_raw_data():
-        
-       root_dir = Load_data.get_root_dir()
-       
-       raw_data = pd.read_csv(root_dir / "raw_data/chess_games.csv")
-       
-       return raw_data
+   root_dir = get_root_dir()
+   
+   raw_data = pd.read_csv(root_dir / "main/filtered_chess_games.csv")
+   
+   return raw_data
+
+
+
+
        
        
     
@@ -29,8 +31,14 @@ class Load_data:
     
 if __name__ == "__main__":
     
-    root = Load_data.get_root_dir()
+    root = get_root_dir()
     
-    raw_data = Load_data.load_raw_data()
+    raw_data = load_raw_data()
     
     print(root)
+    
+    
+    
+
+
+
